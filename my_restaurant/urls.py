55f11f3bf4e433f5eb1ac9dbfd2f6f140f27d20f
blevi3 <blevi333@gmail.com>
2023-01-28 16:57:42 +0100

    modal

--- my_restaurant/urls.py
+++ my_restaurant/urls.py
@@ -1,13 +1,13 @@
 # djangotemplates/example/urls.py
 
-from django.conf.urls import url
 from my_restaurant import views
+from django.urls import path, include
 
 urlpatterns = [
-    url(r'^$', views.HomePageView.as_view(), name='home'), # Notice the URL has been named
-    url(r'^about/$', views.AboutPageView.as_view(), name='about'),
-    url(r'^data/$', views.items_list, name='data'),
+    path('', views.HomePageView.as_view(), name='home'), # Notice the URL has been named
+    path(r'^about/$', views.AboutPageView.as_view(), name='about'),
+    path(r'^data/$', views.items_list, name='data'),
     
-    #url(r'^data/modify$', views.modifydata, name='data'),
-    url(r'^register/$', views.register_request, name="register"),
+    #path(r'^data/modify$', views.modifydata, name='data'),
+    path(r'^register/$', views.register_request, name="register"),
 ]
