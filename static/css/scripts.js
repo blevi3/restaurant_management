bb6767f93cbfb2198cdc04a795e359a03482e362
blevi3 <blevi333@gmail.com>
2023-02-09 18:24:11 +0100

    addElement_modal added

--- static/css/scripts.js
+++ static/css/scripts.js
@@ -1,38 +1,34 @@
-function updateCategory() {
-  var Type = document.getElementById("Type").value;
-  var category = document.getElementById("category");
-  if (Type == "Option 1") {
+function updateCategory(Type, Category) {
+  var type = document.getElementById(Type).value;
+  console.log(type);
+  var category = document.getElementById(Category);
+  $(category).empty();
+  if (type == "Food") {
+     category.innerHTML =
+      "<option>Hamburger</option><option>Hot-Dog</option><option>Snacks</option>";
+  } else if (type == "Drink") {
     category.innerHTML =
-      "<option>Option 1.1</option><option>Option 1.2</option>";
-  } else if (Type == "Option 2") {
-    category.innerHTML =
-      "<option>Option 2.1</option><option>Option 2.2</option>";
+      "<option>Beer</option><option>Wine</option>";
+      console.log(category);
   }
 }
 
-function updateCategory() {
-  var Type = document.getElementById("Type").value;
-  var category = document.getElementById("category");
 
-  var option1 = document.createElement("option");
-  option1.value = "option1";
-  option1.text = "Option 1";
-
-  var option2 = document.createElement("option");
-  option2.value = "option2";
-  option2.text = "Option 2";
-
-  if (Type == "Option 1") {
-    category.appendChild(option1);
-  } else if (Type == "Option 2") {
-    category.appendChild(option2);
-  }
+function openForm(id) {
+  document.getElementById(id).style.display = "block";
 }
+function closeForm(id) {
+  document.getElementById(id).style.display = "none";
+}
+
 
 $(document).on("click", ".modify-button", function () {
+  var tdNameValue = $(this).closest("td").siblings("td#tdTpye").text();
+  var tdNameValue = $(this).closest("td").siblings("td#tdCategory").text();
   var tdPriceValue = $(this).closest("td").siblings("td#tdPrice").text();
   var tdNameValue = $(this).closest("td").siblings("td#tdName").text();
   var inputValue = $(this).closest("tr").find("input#editdata").val();
+  
   document.getElementById("editItemID").value = inputValue;
   document.getElementById("name").value = tdNameValue;
   document.getElementById("price").value = tdPriceValue;
