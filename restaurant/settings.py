2d509be00c0bae12d0e21b07a278c576682a2a72
blevi3 <blevi333@gmail.com>
2023-03-17 11:02:31 +0100

    works

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'f5m=-b(%$72mt)g*g&43ksdcfjff0%y)!@x@nbrjo16nn!@h+e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.31.155', '127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
