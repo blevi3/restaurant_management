9a66a376817a6839b96be2d049ef6d657491033d
blevi3 <blevi333@gmail.com>
2023-10-10 22:29:23 +0200

    req.txt added

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -107,6 +107,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     'my_restaurant.middleware.LocationMiddleware.LocationMiddleware',
 ]
 ALLOWED_COORDINATE = ("47.472484", "19.060647")
